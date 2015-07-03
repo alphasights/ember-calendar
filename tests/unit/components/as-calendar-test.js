ba5d7a219b7b7462e7c37caba2ccf2e6d34ac9a0
Eugenio Depalo <eugenio@alphasights.com>
2015-07-03 10:38:39 +0100

    Fix linter warning

--- tests/unit/components/as-calendar-test.js
+++ tests/unit/components/as-calendar-test.js
@@ -57,7 +57,7 @@ var selectTimeZone = function(name) {
 
   Ember.run(() => {
     $(`.as-calendar-time-zone-option:contains('${name}')`).click();
-  })
+  });
 };
 
 test('Add an occurrence', function(assert) {
