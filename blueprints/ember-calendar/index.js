6bf06e8d2bd6e90e80618c4b0edcde3fead20531
Eugenio Depalo <eugenio@alphasights.com>
2015-07-17 12:07:10 +0100

    Fix blueprint syntax

--- blueprints/ember-calendar/index.js
+++ blueprints/ember-calendar/index.js
@@ -6,12 +6,12 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'ember-cli-moment-shim': '~0.2.0' },
-      { name: 'interact': '~1.2.4' },
-      { name: 'jquery-simulate': '~1.0.1' },
-      { name: 'lodash': '~3.10.0' },
-      { name: 'moment': '~2.10.2' },
-      { name: 'moment-timezone': '~0.4.0' }
+      { name: 'ember-cli-moment-shim', target: '~0.2.0' },
+      { name: 'interact', target: '~1.2.4' },
+      { name: 'jquery-simulate', target: '~1.0.1' },
+      { name: 'lodash', target: '~3.10.0' },
+      { name: 'moment', target: '~2.10.2' },
+      { name: 'moment-timezone', target: '~0.4.0' }
     ]);
   }
 };
