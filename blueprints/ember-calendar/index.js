8e76e4a5d0e409c86c8ffdd642a85e1e19f0a8ff
Eugenio Depalo <eugenio@alphasights.com>
2015-07-17 11:45:20 +0100

    Move npm dependencies out of blueprint

--- blueprints/ember-calendar/index.js
+++ blueprints/ember-calendar/index.js
@@ -4,24 +4,14 @@
 module.exports = {
   normalizeEntityName: function() {},
 
-  beforeInstall: function() {
-    return this.addBowerPackagesToProject([
-      { name: 'moment' },
-      { name: 'ember-cli-moment-shim' },
-      { name: 'moment-timezone' },
-      { name: 'jquery-simulate' },
-      { name: 'interact' },
-      { name: 'lodash' }
-    ]);
-  },
-
   afterInstall: function() {
-    return this.addPackagesToProject([
-      { name: 'broccoli-sass' },
-      { name: 'ember-component-inbound-actions' },
-      { name: 'ember-moment' },
-      { name: 'ember-rl-dropdown' },
-      { name: 'liquid-fire' }
+    return this.addBowerPackagesToProject([
+      { name: 'ember-cli-moment-shim': '~0.2.0' },
+      { name: 'interact': '~1.2.4' },
+      { name: 'jquery-simulate': '~1.0.1' },
+      { name: 'lodash': '~3.10.0' },
+      { name: 'moment': '~2.10.2' },
+      { name: 'moment-timezone': '~0.4.0' }
     ]);
   }
 };
