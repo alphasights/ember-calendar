e5853541f2c824b6fd07fe8275d6124ef4326808
Matteo Depalo <matteodepalo@gmail.com>
2015-08-10 16:46:42 -0400

    Remove ember-cli-moment-shim from bower deps

--- blueprints/ember-calendar/index.js
+++ blueprints/ember-calendar/index.js
@@ -12,7 +12,6 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'ember-cli-moment-shim', target: '0.6.1' },
       { name: 'interact', target: '1.2.4' },
       { name: 'jquery-simulate', target: '1.0.1' },
       { name: 'lodash', target: '3.10.0' },
