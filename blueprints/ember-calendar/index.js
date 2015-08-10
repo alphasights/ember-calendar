a60bb99aee8c21519da6e34d4b7eada6270dfc49
Matteo Depalo <matteodepalo@gmail.com>
2015-08-10 14:40:21 -0400

    Use fixed versions for dependencies

--- blueprints/ember-calendar/index.js
+++ blueprints/ember-calendar/index.js
@@ -4,14 +4,20 @@
 module.exports = {
   normalizeEntityName: function() {},
 
+  beforeInstall: function() {
+    return this.addPackagesToProject([
+      { name: 'broccoli-sass' }
+    ]);
+  },
+
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'ember-cli-moment-shim', target: '~0.2.0' },
-      { name: 'interact', target: '~1.2.4' },
-      { name: 'jquery-simulate', target: '~1.0.1' },
-      { name: 'lodash', target: '~3.10.0' },
-      { name: 'moment', target: '~2.10.2' },
-      { name: 'moment-timezone', target: '~0.4.0' }
+      { name: 'ember-cli-moment-shim', target: '0.2.0' },
+      { name: 'interact', target: '1.2.4' },
+      { name: 'jquery-simulate', target: '1.0.1' },
+      { name: 'lodash', target: '3.10.0' },
+      { name: 'moment', target: '2.10.2' },
+      { name: 'moment-timezone', target: '0.4.0' }
     ]);
   }
 };
