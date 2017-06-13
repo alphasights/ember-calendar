36e8f7336c43d50e2e971cdf9129200d764e6a6e
Will Raxworthy <me@willrax.com>
2017-06-13 12:10:50 +0100

    use ember test selectors to test

--- tests/unit/components/as-calendar-test.js
+++ tests/unit/components/as-calendar-test.js
@@ -2,10 +2,10 @@ import hbs from 'htmlbars-inline-precompile';
 import { test, moduleForComponent } from 'ember-qunit';
 import Ember from 'ember';
 import moment from 'moment';
+import testSelector from 'ember-test-selectors';
 
 import {
   timeSlotHeight,
-  dayWidth,
   selectTime,
   resizeOccurrence,
   dragOccurrence,
@@ -47,23 +47,13 @@ test('Add an occurrence', function(assert) {
       onRemoveOccurrence=(action "calendarRemoveOccurrence")}}
   `);
 
-  assert.equal(Ember.$('.as-calendar-occurrence').length, 0,
-    'it shows an empty calendar'
-  );
+  assert.equal(Ember.$('.as-calendar-occurrence').length, 0, 'it shows an empty calendar');
 
   selectTime({ day: 0, timeSlot: 0 });
 
-  assert.equal(Ember.$('.as-calendar-occurrence').length, 1,
-    'it adds the occurrence to the calendar'
-  );
-
-  assert.ok(this.get('occurrences.firstObject').startsAt instanceof Date,
-    'startsAt is a Date'
-  );
-
-  assert.ok(this.get('occurrences.firstObject').endsAt instanceof Date,
-    'endsAt is a Date'
-  );
+  assert.equal(Ember.$('.as-calendar-occurrence').length, 1, 'it adds the occurrence to the calendar');
+  assert.ok(this.get('occurrences.firstObject').startsAt instanceof Date, 'startsAt is a Date');
+  assert.ok(this.get('occurrences.firstObject').endsAt instanceof Date, 'endsAt is a Date');
 });
 
 test('Remove an occurrence', function(assert) {
@@ -81,17 +71,13 @@ test('Remove an occurrence', function(assert) {
 
   selectTime({ day: 0, timeSlot: 0 });
 
-  assert.equal(Ember.$('.as-calendar-occurrence').length, 1,
-    'it adds the occurrence to the calendar'
-  );
+  assert.equal(Ember.$('.as-calendar-occurrence').length, 1, 'it adds the occurrence to the calendar');
 
   Ember.run(() => {
     Ember.$('.as-calendar-occurrence .as-calendar-occurrence__remove').click();
   });
 
-  assert.equal(Ember.$('.as-calendar-occurrence').length, 0,
-    'it removes the occurrence from the calendar'
-  );
+  assert.equal(Ember.$('.as-calendar-occurrence').length, 0, 'it removes the occurrence from the calendar');
 });
 
 
@@ -122,11 +108,6 @@ test('Resize an occurrence', function(assert) {
 });
 
 test('Drag an occurrence', function(assert) {
-  const pixelAccuracy = 5;
-  let assertAlmostEqual = function(first, second, message) {
-    assert.ok(Math.abs(first - second) < pixelAccuracy, message);
-  };
-
   this.render(hbs`
     {{as-calendar
       title="Ember Calendar"
@@ -141,31 +122,14 @@ test('Drag an occurrence', function(assert) {
 
   selectTime({ day: 0, timeSlot: 0 });
 
-  assert.equal(this.$('.as-calendar-occurrence').length, 1,
-    'it adds the occurrence to the calendar'
-  );
+  assert.equal(this.$('.as-calendar-occurrence').length, 1, 'it adds the occurrence to the calendar');
+  let inMonday = this.$(testSelector('day-id', '0')).find('.as-calendar-occurrence').length === 1;
+  assert.ok(inMonday);
 
   dragOccurrence(this.$('.as-calendar-occurrence'), { days: 2, timeSlots: 4 });
 
-  var $occurrence = this.$('.as-calendar-occurrence');
-
-  var dayOffset = $occurrence.offset().left -
-    this.$('.as-calendar-timetable-content').offset().left;
-
-  var timeSlotOffset = $occurrence.offset().top -
-    this.$('.as-calendar-timetable-content').offset().top;
-
-  assertAlmostEqual(dayOffset, dayWidth() * 2,
-    'it drags the occurrence to the correct day'
-  );
-
-  assertAlmostEqual(timeSlotOffset, timeSlotHeight() * 4,
-    'it drags the occurrence to the correct timeslot'
-  );
-
-  assertAlmostEqual($occurrence.height(), timeSlotHeight() * 2,
-    'it keeps the duration of the occurrence'
-  );
+  let inWednesday = this.$(testSelector('day-id', '2')).find('.as-calendar-occurrence').length === 1;
+  assert.ok(inWednesday);
 });
 
 test('Change time zone', function(assert) {
