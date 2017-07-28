9e23ff94df9d9ec54e87defddb3145c84a68a83e
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-07-28 12:50:40 -0400

    Replace bower/fontawesome with npm package

--- index.js
+++ index.js
@@ -9,34 +9,6 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    var options = app.options.emberCalendar || {};
-
-    if (!('includeFontAwesomeAssets' in options)) {
-      options.includeFontAwesomeAssets = true;
-    }
-
-    if (options.includeFontAwesomeAssets) {
-      app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.ttf'), {
-        destDir: 'fonts'
-      });
-
-      app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.woff'), {
-        destDir: 'fonts'
-      });
-
-      app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.woff2'), {
-        destDir: 'fonts'
-      });
-
-      app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.svg'), {
-        destDir: 'fonts'
-      });
-
-      app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.eot'), {
-        destDir: 'fonts'
-      });
-    }
-
     app.import(path.join(app.bowerDirectory, 'interact/interact.js'));
     app.import('vendor/shims/interact.js');
 
