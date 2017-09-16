6782526a099ffc953e98a4e36e1b07a6a4d070e1
Tony Miller <mcfiredrill@gmail.com>
2017-09-16 11:37:39 +0900

    get it hopefully working with fastboot

--- index.js
+++ index.js
@@ -2,14 +2,27 @@
 'use strict';
 
 const path = require('path');
+const fastbootTransform = require('fastboot-transform');
 
 module.exports = {
   name: 'ember-calendar',
 
+  options: {
+    nodeAssets: {
+      'interactjs': {
+        import: {
+          include: ['interactjs'],
+          processTree(input) {
+            return fastbootTransform(input);
+          }
+        }
+      }
+    }
+  },
+
   included: function(app) {
     this._super.included(app);
 
-    app.import(path.join(path.dirname(require.resolve('interactjs')), 'interact.js'));
     app.import('vendor/shims/interact.js');
 
     app.import('vendor/jstz.js');
