b5b35256f29151559d87d3741ad2e950e3ef11c1
Tony Miller <mcfiredrill@gmail.com>
2018-10-07 11:15:24 +0900

    use ember-font-awesome

--- blueprints/ember-calendar/index.js
+++ blueprints/ember-calendar/index.js
@@ -8,7 +8,10 @@ module.exports = {
     var self = this;
 
     return self.addAddonsToProject({
-      packages: [{ name: 'ember-moment', target: '7.3.0' }]
+      packages: [
+        { name: 'ember-moment', target: '7.3.0' },
+        { name: 'ember-font-awesome', target: '4.0.0' }
+      ]
     }).then(function() {
       return self.addPackagesToProject([
         { name: 'ember-cli-sass', target: '5.3.1' },
@@ -18,8 +21,6 @@ module.exports = {
     }).then(function() {
       return self.addBowerPackagesToProject([
         { name: 'interact', target: 'unstable' },
-        { name: 'jquery-simulate', target: '1.0.1' },
-        { name: 'fontawesome', target: '~4.5.0'},
         { name: 'jquery-simulate', target: '1.0.1' }
       ]);
     });
--- index.js
+++ index.js
@@ -11,33 +11,6 @@ module.exports = {
 
     var options = app.options.emberCalendar || {};
 
-    if (!('includeFontAwesomeAssets' in options)) {
-      options.includeFontAwesomeAssets = true;
-    }
-
-
-    if (options.includeFontAwesomeAssets) {
-      app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.ttf'), {
-        destDir: 'fonts'
-      });
-
-      app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.woff'), {
-        destDir: 'fonts'
-      });
-
-      app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.woff2'), {
-        destDir: 'fonts'
-      });
-
-      app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.svg'), {
-        destDir: 'fonts'
-      });
-
-      app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.eot'), {
-        destDir: 'fonts'
-      });
-    }
-
     if (app.env === 'test') {
       app.import(path.join(app.bowerDirectory, 'jquery-simulate/jquery.simulate.js'), {
         type: 'test'
