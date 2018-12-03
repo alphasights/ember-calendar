72f15a79fa79861b68fbbbbe1806dd0cb456eb99
Tony Miller <mcfiredrill@gmail.com>
2018-12-03 19:42:04 +0900

    remove jquery usage for test helpers

--- addon/test-helpers/all.js
+++ addon/test-helpers/all.js
@@ -1,75 +1,67 @@
 import { run } from '@ember/runloop';
-import $ from 'jquery';
+import { click, find, triggerEvent } from '@ember/test-helpers';
 import { drag } from 'ember-drag-drop/test-support/helpers/drag-drop';
 
-var timeSlotHeight = function() {
-  return $('.as-calendar-timetable-content')
+const timeSlotHeight = function() {
+  return find('.as-calendar-timetable-content')
     .find('.as-calendar-timetable__slot-item:first')
-    .height();
+    .offsetHeight;
 };
 
-var dayWidth = function() {
-  var $content = $('.as-calendar-timetable-content');
-  return $content.width() / $content.find('.as-calendar-timetable__day').length;
+const dayWidth = function() {
+  const $content = find('.as-calendar-timetable-content');
+  return $content.offsetWidth() / $content.findAll('.as-calendar-timetable__day').length;
 };
 
-var pointForTime = function(options) {
-  var $target = $('.as-calendar-timetable-content');
-  var offsetX = options.day * dayWidth();
-  var offsetY = options.timeSlot * timeSlotHeight();
-  var pageX = $target.offset().left + offsetX;
-  var pageY = $target.offset().top + offsetY;
+const pointForTime = function(options) {
+  const $target = find('.as-calendar-timetable-content');
+  const offsetX = options.day * dayWidth();
+  const offsetY = options.timeSlot * timeSlotHeight();
+  const pageX = $target.getBoundingClientRect().left + offsetX;
+  const pageY = $target.getBoundingClientRect().top + offsetY;
 
   return {
     pageX: pageX,
     pageY: pageY,
-    clientX: pageX - $(document).scrollLeft(),
-    clientY: pageY - $(document).scrollTop()
+    clientX: pageX - document.documentElement.scrollLeft,
+    clientY: pageY - document.documentElement.scrollTop
   };
 };
 
-var selectTime = function(options) {
+const selectTime = function(options) {
   run(() => {
-    var $target = $('.as-calendar-timetable-content');
-    var point = pointForTime(options);
-    var event = $.Event('click');
+    const $target = find('.as-calendar-timetable-content');
+    const point = pointForTime(options);
 
-    event.pageX = point.pageX;
-    event.pageY = point.pageY;
-
-    $target.trigger(event);
+    triggerEvent($target, 'click', point);
   });
 };
 
-var resizeOccurrence = async function(occurrence, options) {
+const resizeOccurrence = async function(occurrence, options) {
   let dragSelector = occurrence.find('.as-calendar-occurrence__resize-handle');
   await drag(dragSelector, {
     dropEndOptions: {
       pageX: 0,
-      pageY: options.timeSlots * timeSlotHeight() + occurrence.height()
+      pageY: options.timeSlots * timeSlotHeight() + occurrence.offsetHeight
     }
   });
 };
 
-var dragOccurrence = async function(occurrence, options) {
+const dragOccurrence = async function(occurrence, options) {
   await drag(occurrence, {
     dropEndOptions: {
       pageX: options.days * dayWidth(),
-      pageY: options.timeSlots * timeSlotHeight() + occurrence.height()
+      pageY: options.timeSlots * timeSlotHeight() + occurrence.offsetHeight
     }
   });
 };
 
-var selectNextWeek = function() {
-  run(() => {
-    $('button.as-calendar-header__nav-group-action--next-week').click();
-  });
+const selectNextWeek = async function() {
+  await click('button.as-calendar-header__nav-group-action--next-week');
 };
 
-var selectPreviousWeek = function() {
-  run(() => {
-    $('button.as-calendar-header__nav-group-action--previous-week').click();
-  });
+const selectPreviousWeek = async function() {
+ await click('button.as-calendar-header__nav-group-action--previous-week');
 };
 
 export {
